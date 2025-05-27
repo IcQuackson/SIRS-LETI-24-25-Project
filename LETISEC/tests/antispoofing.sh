2fca68f181179aa9e333fabf7c85492e0210a4df
IcQuackson <pedrorenato1998@gmail.com>
2025-05-27 14:39:07 +0100

    replace stateful fw rules with stateless and fix rules

--- LETISEC/tests/antispoofing.sh
+++ LETISEC/tests/antispoofing.sh
@@ -6,7 +6,7 @@ echo "Testing antispoofing using nemesis (correct flags: -S source, -D destinati
 # NY: somepc tries to spoof a source IP
 # to ping pcb with spoofed src IP from pca
 echo "-> NY test (somepc → 6.6.6.6, spoof src=14.4.3.1)"
-kexec pca nemesis tcp -S 14.4.3.1 -D 14.4.3.2 &>/dev/null \
+kexec somepc nemesis tcp -S 14.4.3.1 -D 14.4.3.2 &>/dev/null \
   && { echo "❌ NY antispoofing failed - spoofed packet sent"; exit 1; } \
   || echo "✅ NY antispoofing passed - spoofed packet dropped"
 
@@ -19,7 +19,7 @@ kexec pca nemesis tcp -S 8.8.8.8 -D 6.6.6.6 > /dev/null 2>&1 \
 # Oeiras: somepc tries to spoof a source IP
 # to ping superpc with spoofed src IP from webserver
 echo "-> Oeiras test (somepc → 6.6.6.6, spoof src=10.2.3.1)"
-kexec pc1 nemesis tcp -S 10.2.3.1 -D 10.2.5.1 &>/dev/null \
+kexec somepc nemesis tcp -S 10.2.3.1 -D 10.2.5.1 &>/dev/null \
   && { echo "❌ Oeiras antispoofing failed - spoofed packet sent"; exit 1; } \
   || echo "✅ Oeiras antispoofing passed - spoofed packet dropped"
 
